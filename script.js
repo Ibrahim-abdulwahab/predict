0d7db9c7166c0b1900bd578482cccaabd9c86f63
Ibrahim Abdulwahab <ibrahim.abdulwahab1997@gmail.com>
2022-09-10 23:30:04 +0300

    presing the button predicts gender

--- script.js
+++ script.js
@@ -7,6 +7,35 @@ window.addEventListener('load', function(){
                 dog_result.innerHTML = `<img src=${data.message} alt="dog" width=100% height=100% style="border-top-left-radius: 9em;border-bottom-left-radius: 9em;"/>`
             });
     }
+    document.getElementById("btn").addEventListener("click",getInfo);
+    async function getInfo(){
+        const name= document.getElementById("fname").value;
+
+        if (!name){
+            alert("Please Enter a name");
+            return;   
+        }
+
+        const  gender=document.getElementById("gender");
+        const  age=document.getElementById("age");
+        //const  nationality=document.getElementById("nationality");
+        
+        function getGender(){
+            fetch(`https://api.genderize.io/?name=${name}`)
+                .then(res => res.json())
+                .then(data => {
+                    gender.innerHTML = `Your gender is ${data.gender}`
+                })
+        }
+        getGender()
+        function getAge(){
+            fetch(`https://api.agify.io/?name=${name}`)
+                .then(res => res.json())
+                .then(data => {
+                    age.innerHTML = `Your age is ${data.age}`
+                })
+        }
+    }
     
     randomDogPic()
 })
