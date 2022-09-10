779eec3e6eebbdbf0c27cbfa8df7ef2507b3968c
Ibrahim Abdulwahab <ibrahim.abdulwahab1997@gmail.com>
2022-09-10 14:03:03 +0300

    generates random dog pic

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-window.onload=()=>{
-    console.log("Hi")
-}
+window.addEventListener('load', function(){
+    const dog_result=document.getElementById("dog");
+    function randomDogPic() {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => {
+                dog_result.innerHTML = `<img src=${data.message} alt="dog" width=100% height=100% style="border-top-left-radius: 9em;border-bottom-left-radius: 9em;"/>`
+            });
+    }
+    
+    randomDogPic()
+})
